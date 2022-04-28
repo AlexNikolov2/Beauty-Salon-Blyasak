e240ffae8adc6e532a85b8ec2b94e947fc1a157a
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-28 23:10:01 +0300

    delete, aint gonna work that way

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const port = 3000;
-
-http.get('http://localhost:3000/', (res) => {
-    console.log(`Got response: ${res.statusCode}`);
-}).on('error', (err) => {
-    console.log(`Got error: ${err.message}`);
-});
-http.createServer().listen(port);
